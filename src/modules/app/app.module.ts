d12ff96455af9c36af037978b9d4e8df4f421d41
flowwishthebest <togowiththeflow@gmail.com>
2020-01-22 13:48:41 +0300

    Added middleware

---
+++ src/modules/app/app.module.ts
@@ -0,0 +1,18 @@
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoggerMiddleware } from './common/middlewares/logging.middleware';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [CommonModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(AppController);
+  }
+}
---
+++ src/modules/app/common/middlewares/logging.middleware.ts
@@ -0,0 +1,14 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: FastifyRequest, res: FastifyReply<any>, next: () => void) {
+    //tslint:disable
+    // console.log('Request...', req);
+
+    console.log(req.body, req.query, req.params);
+
+    next();
+  }
+}
