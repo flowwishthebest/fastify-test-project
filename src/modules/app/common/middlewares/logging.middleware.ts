54b1eec1443884902019814f8cf78a222908688e
Ilya P <32912855+flowwishthebest@users.noreply.github.com>
2020-01-22 15:28:27 +0300

    Update logging.middleware.ts

--- src/modules/app/common/middlewares/logging.middleware.ts
+++ src/modules/app/common/middlewares/logging.middleware.ts
@@ -4,7 +4,7 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: FastifyRequest, res: FastifyReply<any>, next: () => void) {
-    //tslint:disable
+    // tslint:disable
     // console.log('Request...', req);
 
     console.log(req.body, req.query, req.params);
